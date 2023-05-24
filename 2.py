57add4996f60371d38943c6dd1d9b496bb245941
Moraci Oliveira <moraci.oliveira@attackiq.com>
2023-05-24 11:28:07 -0300

    Added error message to assert statement

--- 2.py
+++ 2.py
@@ -6,4 +6,4 @@
 countries = ['POL', 'ENG', 'GER', 'USA', 'ITA']
 is_italy = 'ITA' in countries
 
-assert is_italy
+assert is_italy, 'Italy not found in the countries list.'  #Added error message to assert statement
