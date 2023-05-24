5ad9d84c0c72d3433317b26b72c5486b6d7d96af
Moraci Oliveira <moraci.oliveira@attackiq.com>
2023-05-24 11:26:39 -0300

    exercise completed

---
+++ 2.py
@@ -0,0 +1,9 @@
+#Assert the is_italy variable using the assert statement:
+# countries = ['POL', 'ENG', 'GER', 'USA', 'ITA']
+# is_italy = 'ITA' in countries
+
+# Solution
+countries = ['POL', 'ENG', 'GER', 'USA', 'ITA']
+is_italy = 'ITA' in countries
+
+assert is_italy
