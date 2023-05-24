3a1e66a5468a1913df0f9b2a12e230dc84a0b114
Moraci Oliveira <moraci.oliveira@attackiq.com>
2023-05-24 12:17:02 -0300

    addition of exercise 6

--- 5.py
+++ 5.py
@@ -1,29 +1,20 @@
-# The implementation of the max_min_diff() function is given:
-#
-# def max_min_diff(numbers):
-#     """
-#     Calculates the difference between the maximum and minimum number
-#     in the given list.
-#
-#     :param numbers: A list of numbers.
-#     :return: The difference between the maximum and minimum number
-#     in the list.
-#     """
-#     return max(numbers) - min(numbers)
-#
-# Modify the implementation of the max_min_diff() function.
-# By using the assert statement inside this function,
-# add the ability to check the length of the numbers argument
-# before returning the result. If the length of the numbers
-# object is 0 raise the AssertionError with message:
-#
-# 'The input list cannot be empty.'
-#
-# Otherwise, it returns the correct result
-# (the difference between the highest and lowest value for numbers).
-#
-# In case the module with the solution is run directly,
-# call the max_min_diff() function passing an empty list.
+"""
+The implementation of the max_min_diff() function is given:
+
+Modify the implementation of the max_min_diff() function.
+By using the assert statement inside this function,
+add the ability to check the length of the numbers argument
+before returning the result. If the length of the numbers
+object is 0 raise the AssertionError with message:
+
+'The input list cannot be empty.'
+
+Otherwise, it returns the correct result
+(the difference between the highest and lowest value for numbers).
+
+In case the module with the solution is run directly,
+call the max_min_diff() function passing an empty list.
+"""
 
 def max_min_diff(numbers):
     """
