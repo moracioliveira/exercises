7b7208857452e194a2577eb73ae7e61de351c1f8
Moraci Oliveira <moraci.oliveira@attackiq.com>
2023-05-24 23:31:40 -0300

    addition of 12

--- 11.py
+++ 11.py
@@ -32,7 +32,18 @@ def factorial(n: int) -> int:
         return n * factorial(n - 1)
 
 # Enter your solution here
+# Test that factorial of 0 is 1
 assert factorial(0) == 1
+
+# Test that factorial of 1 is 1
 assert factorial(1) == 1
+
+# Test that factorial of 5 is 120
 assert factorial(5) == 120
-assert ValueError(factorial(-1))
+
+# Test that factorial of a negative number raises a ValueError
+try:
+    factorial(-1)
+    assert False  # This line should not be reached
+except ValueError:
+    pass
