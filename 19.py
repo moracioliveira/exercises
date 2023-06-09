4765e769204c4f39580befd4428855b196a841cd
Moraci Oliveira <moraci.oliveira@attackiq.com>
2023-06-08 22:09:32 -0300

    addition of 19 to 20

---
+++ 19.py
@@ -0,0 +1,35 @@
+"""
+Using the unittest framework create a TestLower class
+that inherits from the unittest.TestCase class and implements the following two tests:
+
+test_lower()
+
+test that checks if the code 'Joe.Smith@mail.com'.lower() returns string 'joe.smith@mail.com'
+
+test_is_lower()
+
+test that checks if the code 'joe.smith@mail.com'.islower() returns the boolean value True
+
+test that checks if the code 'Joe.Smith@mail.com'.islower() returns the boolean value False
+
+You only need to define the class and the appropriate tests.
+During the solution verification, the tests are run and in case of any errors,
+the test report will be printed to the console.
+"""
+
+import unittest
+
+
+# Enter your solution here
+class TestLower(unittest.TestCase):
+
+    def test_lower(self):
+        self.assertEqual('Joe.Smith@mail.com'.lower(), 'joe.smith@mail.com')
+
+    def test_is_lower(self):
+        self.assertTrue('joe.smith@mail.com'.islower())
+        self.assertFalse('Joe.Smith@mail.com'.islower())
+
+
+if __name__ == '__main__':
+    unittest.main()
